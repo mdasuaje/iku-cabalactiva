b3d5b55f6565df2b3a74cd21748860326d268eda
mdasuaje <mdasuaje@protonmail.com>
2025-08-23 02:35:32 +0000

    dos archivos agregados index

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>¡Bienvenido a IKU Cábala Activa!</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
