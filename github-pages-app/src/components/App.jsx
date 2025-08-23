2f420ee25df03b75517a736ae741c8a28933ecbf
mdasuaje <mdasuaje@protonmail.com>
2025-08-23 02:38:33 +0000

    testing1 creación workspace

---
+++ github-pages-app/src/components/App.jsx
@@ -0,0 +1,13 @@
+// filepath: /github-pages-app/github-pages-app/src/components/App.jsx
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>¡Bienvenido a IKU Cábala Activa!</h1>
+      {/* Aquí puedes agregar más componentes y lógica para tu aplicación */}
+    </div>
+  );
+}
+
+export default App;
